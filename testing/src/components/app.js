67ecde5e03f771bf327b93956a24650dcfe57d96
Jeff Quinn <ns8riders@me.com>
2017-07-01 21:51:46 -0600

    Starting AdvancedReduxCode/testing

--- testing/src/components/app.js
+++ testing/src/components/app.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Component } from 'react';
-import CommentBox from './comment_box';
-import CommentList from './comment_list';
+// import CommentBox from './comment_box';
+// import CommentList from './comment_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <CommentBox />
-        <CommentList />
+        React simple starter
       </div>
     );
   }
--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,19 +1,18 @@
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-// Use 'describe' to group together similar tests
+// Use 'describe' to group together similar tests (good to use the component name you wish to test)
 describe('App', () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  // Use 'it' to test a single attribute of a target
+  it('show the correct text', () => {
+
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple starter');
+
   });
 
-  it('shows a comment box', () => {
-    expect(component.find('.comment-box')).to.exist;
-  });
-
-  it('shows a comment list', () => {
-    expect(component.find('.comment-list')).to.exist;
-  });
 });
